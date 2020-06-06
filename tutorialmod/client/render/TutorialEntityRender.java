4db674c78c4fd3e22d0c2def58ac8ed44f643f6c
bobsmith7557 <65967322+bobsmith7557@users.noreply.github.com>
2020-06-06 16:23:43 -0700

    Update TutorialEntityRender.java

--- tutorialmod/client/render/TutorialEntityRender.java
+++ tutorialmod/client/render/TutorialEntityRender.java
@@ -1,5 +1,7 @@
 package tutorialmod.client.render;
-
+import tutorialmod.TutorialModRegistries;
+import tutorialmod.client.models.TutorialEntityModel;
+import tutorialmod.entities.TutorialEntity;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.LivingRenderer;
@@ -7,30 +9,28 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
-import tutorialmod.TutorialModRegistries;
-import tutorialmod.client.models.TutorialEntityModel;
-import tutorialmod.entities.TutorialEntity;
 
-// this renders the entity, we will need to add to this further
 @OnlyIn(Dist.CLIENT)
-public class TutorialEntityRender extends LivingRenderer<TutorialEntity, TutorialEntityModel> {
-
-	public TutorialEntityRender(EntityRendererManager manager) {
-		super(manager, new TutorialEntityModel(), 10);
-		
+public class TutorialEntityRender extends LivingRenderer<TutorialEntity, TutorialEntityModel>
+{
+	public TutorialEntityRender(EntityRendererManager manager) 
+	{
+		super(manager, new TutorialEntityModel(), 0f);
 	}
-	
+
 	@Override
-	protected ResourceLocation getEntityTexture(TutorialEntity entity) {
+	protected ResourceLocation getEntityTexture(TutorialEntity entity) 
+	{
 		return TutorialModRegistries.location("textures/entity/tutorial_entity.png");
 	}
-	
-	public static class RenderFactory implements IRenderFactory<TutorialEntity>{
 
+	public static class RenderFactory implements IRenderFactory<TutorialEntity>
+	{
 		@Override
-		public EntityRenderer<? super TutorialEntity> createRenderFor(EntityRendererManager manager) {
-	
+		public EntityRenderer<? super TutorialEntity> createRenderFor(EntityRendererManager manager) 
+		{
 			return new TutorialEntityRender(manager);
 		}
+		
 	}
 }
