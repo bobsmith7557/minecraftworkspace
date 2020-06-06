86162d160722a48b2a23102b1be00810c8cecefd
bobsmith7557 <65967322+bobsmith7557@users.noreply.github.com>
2020-06-06 16:38:37 -0700

    Update TutorialRenderRegistry.java

--- tutorialmod/client/render/TutorialRenderRegistry.java
+++ tutorialmod/client/render/TutorialRenderRegistry.java
@@ -1,15 +1,15 @@
 package tutorialmod.client.render;
 
+import tutorialmod.entities.TutorialEntity;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
-import tutorialmod.entities.TutorialEntity;
 
-// this registers the renderer of the entity
 @OnlyIn(Dist.CLIENT)
-public class TutorialRenderRegistry {
-	
-	public static void registerEntityRenderers() {
-		RenderingRegistry.registerEntityRenderingHandler(TutorialEntity.class,new TutorialEntityRender.RenderFactory());
+public class TutorialRenderRegistry 
+{
+	public static void registryEntityRender()
+	{
+		RenderingRegistry.registerEntityRenderingHandler(TutorialEntity.class, new TutorialEntityRender.RenderFactory());
 	}
 }
